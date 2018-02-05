1a5cc799149998993ebe5ed4b6176676aafc3fe5
Valery-Daineko <36156543+Valery-Daineko@users.noreply.github.com>
2018-02-05 15:05:32 +0200

    Initial commot

---
+++ avto/js/chast.js
@@ -0,0 +1,4 @@
+$('.fofo').slick({
+    dots: true
+});
+				
---
+++ avto/js/js.js
@@ -0,0 +1,10 @@
+$('.sl').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true
+});
+
+$('.slik').slick({
+    dots: true
+});
